52d34dda1a3214127ac21420285031d8b881b061
Gospodin-DG <gospodin-dg@yandex.ru>
2022-03-05 18:12:01 +0300

    Create imageButton previous, next, add list of Questions

--- app/src/main/java/com/example/smartquiz/MainActivity.kt
+++ app/src/main/java/com/example/smartquiz/MainActivity.kt
@@ -1,50 +1,103 @@
 package com.example.smartquiz
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var questionText: TextView
     private lateinit var btnTrue: Button
     private lateinit var btnFalse: Button
+    private lateinit var btnNext: ImageButton
+    private lateinit var btnPrevious: ImageButton
     private var currentQuestion: Int = 0
+    private val bankQuestion = listOf<Question>(
+        Question(R.string.question_0, true),
+        Question(R.string.question_1, true),
+        Question(R.string.question_2, true),
+        Question(R.string.question_3, false),
+        Question(R.string.question_4, true),
+        Question(R.string.question_5, true),
+        Question(R.string.question_6, true)
+    )
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
-        createQuestions()
-
     }
 
     fun init() {
         questionText = findViewById(R.id.question_text)
         btnTrue = findViewById(R.id.btn_true)
         btnFalse = findViewById(R.id.btn_false)
+        btnNext = findViewById(R.id.btn_next)
+        btnPrevious = findViewById(R.id.btn_previous)
+        questionText.setText(bankQuestion[currentQuestion].questionText)
+
         btnTrue.setOnClickListener{view: View ->
-            Toast.makeText(this, R.string.true_answer, Toast.LENGTH_SHORT).also {
-                it.setGravity(Gravity.TOP, 0,0)
-                it.show()
-            }
+           checkAnswer(true)
         }
         btnFalse.setOnClickListener{view: View ->
-            Toast.makeText(this, R.string.false_answer, Toast.LENGTH_SHORT).also {
-                it.setGravity(Gravity.TOP, 0,0)
-                it.show()
-            }
+            checkAnswer(false)
+        }
+
+        btnNext.setOnClickListener{view: View ->
+            updateQuestion(view)
+        }
+
+        btnPrevious.setOnClickListener{view: View ->
+            updateQuestion(view)
+        }
+
+        questionText.setOnClickListener{view: View ->
+            updateQuestion(view)
         }
     }
 
-    fun createQuestions() {
+    private fun updateQuestion(view: View) {
+        val view_id = view.id
+        when(view_id){
+            R.id.question_text -> nextQuestion()
+            R.id.btn_next -> nextQuestion()
+            R.id.btn_previous -> previousQuestion()
+        }
+        //currentQuestion = (currentQuestion + 1) % bankQuestion.size
+        questionText.setText(bankQuestion[currentQuestion].questionText)
+    }
 
+    private fun nextQuestion(){
+        if (currentQuestion < (bankQuestion.size-1)){
+            currentQuestion = currentQuestion + 1
+        } else {
+            currentQuestion = 0
+        }
+    }
+
+    private fun previousQuestion(){
+        if (currentQuestion > 0){
+            currentQuestion = currentQuestion - 1
+        } else {
+            currentQuestion = bankQuestion.size - 1
+        }
+    }
+
+    private fun checkAnswer(userAnswer: Boolean ){
+        val trueAnswerQuestion = bankQuestion[currentQuestion].trueAnswer
+        if (userAnswer == trueAnswerQuestion){
+            Toast.makeText(this, R.string.true_answer, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, R.string.false_answer, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
+
 
 
